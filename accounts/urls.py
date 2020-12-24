29553886b715516485309875ee7ab462e6664612
shreyasrami <shreyasrami24.sr@gmail.com>
2020-12-24 18:08:47 +0530

    Add Forgot Password Feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
-    path('register/',views.register,name='register')
+    path('register/',views.register,name='register'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+
 
 ]
