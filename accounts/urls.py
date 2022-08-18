84fbb8ec877c6d140d8db0cc3fbacb14d2cceafa
shreyasrami <shreyasrami24.sr@gmail.com>
2022-08-18 12:32:13 +0530

    Fix bug in password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete')
 
 
 ]
