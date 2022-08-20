237d368b24495291d8767d7999b5a970293180db
shreyasrami <shreyasrami24.sr@gmail.com>
2022-08-20 20:55:59 +0530

    Change DB settings

--- insta/settings.py
+++ insta/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : config('DB_NAME'),
-        'USER' : config('DB_USER'),
-        'PASSWORD' : config('DB_PASSWORD'),
-        'HOST' : config('DB_HOST')
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE' : 'django.db.backends.postgresql',
+#         'NAME' : config('DB_NAME'),
+#         'USER' : config('DB_USER'),
+#         'PASSWORD' : config('DB_PASSWORD'),
+#         'HOST' : config('DB_HOST')
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
