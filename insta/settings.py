2c2b7a89ada71bd723968f9e795ae6a37ce56770
shreyasrami <shreyasrami24.sr@gmail.com>
2022-12-08 19:34:07 +0530

    Modify deploy settings

--- insta/settings.py
+++ insta/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['socio-insta.herokuapp.com']
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
